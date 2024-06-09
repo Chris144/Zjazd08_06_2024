05b3b6bee1a8d7134c055efd5f042622b6a21766
Chris144 <krzysztofktly@gmail.com>
2024-06-09 09:25:50 +0200

    dodanie

--- test_1/test_1.py
+++ test_1/test_1.py
@@ -1,6 +1,7 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 class BaseTest(unittest.TestCase):
@@ -13,7 +14,14 @@ class BaseTest(unittest.TestCase):
         self.driver.maximize_window()
         self.driver.get("http://seleniumdemo.com/")
         self.driver.implicitly_wait(5)
+        text = self.driver.find_element(By.CLASS_NAME, 'sek-module-inner')
+        self.assertEqual(
+            "Design your own space",
+            text.text)
+        print(text.text)
 
     def tearDown(self):
         self.driver.quit()
 
+if __name__ == '__main__':
+    unittest.main()
